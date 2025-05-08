710140a838a0e71b081139abaafe6159f057abbc
Josefum1 <juninho.jads@gmail.com>
2025-05-08 17:19:03 -0300

    Initial commit

---
+++ src/main/java/me/dio/dio_desafio_de_codigo/DioDesafioDeCodigoApplication.java
@@ -0,0 +1,13 @@
+package me.dio.dio_desafio_de_codigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesafioDeCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesafioDeCodigoApplication.class, args);
+	}
+
+}
